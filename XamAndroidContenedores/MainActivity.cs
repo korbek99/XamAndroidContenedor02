67a092c8c40ed84393dd2994f81f87b1a1ca5534
korbek100 <korbek100@gmail.com>
2018-05-06 01:56:39 -0300

    first commit

---
+++ XamAndroidContenedores/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamAndroidContenedores
+{
+    [Activity(Label = "XamAndroidContenedores", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
+
